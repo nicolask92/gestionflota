6f3b524bd35ccc005c8f5b967009586cdfdcf54b
Nicolas Kloster Notebook <niko.klos@gmail.com>
2019-12-30 13:47:45 -0300

    Agregando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,13 @@
 <div id="bootstrap-iso form" class="container-xl">
 
     <form autocomplete="off" method="POST">
-        <div class="row">
-            <div id="app">
+        <div class="row" id="app">
+              <div>
+                  <vuejs-datepicker :typeable="true" :language="es" :value="dateVal" :format="dateFormat" class="col" name="fecha_c" placeholder="COMIENZO"></vuejs-datepicker>
               </div>
-            <div class="col">
-                <input class="form-control" id="date1" name="date1" value="{{ old('date') }}" placeholder="COMIENZO" type="text"/>
-            </div>
-            <div class="col">
-                    <input class="form-control" id="date" name="date2" value="{{ old('date') }}" placeholder="FIN" type="text"/>
-            </div>
+              <div>
+                <vuejs-datepicker :typeable="true" :language="es" :value="dateVal" :format="dateFormat" class="col" name="fecha_f" placeholder="FIN"></vuejs-datepicker>
+              </div>
             <div class="col">
                     <input type="text" class="form-control" placeholder="PAX">
             </div>
@@ -36,7 +34,35 @@
 
 @section('script')
 
+<script src="https://unpkg.com/vue"></script>
+<script src="https://unpkg.com/vuejs-datepicker"></script>
+<script src="https://unpkg.com/vuejs-datepicker/dist/locale/translations/es.js"></script>
+<script>
+const app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      es: vdp_translation_es.js,
+      dateFormat : 'dd-MM-yyyy',
+      dateVal : new Date()
+    }
+  },
+  components: {
+  	vuejsDatepicker
+  }
+});
+
+
+</script> 
+<script
+src="https://code.jquery.com/jquery-3.4.1.min.js"
+integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+crossorigin="anonymous"></script>
 <script type="application/javascript">
+$(document).ready(function(){
+        $("#app > div:nth-child(1) > div > div:nth-child(1) > input").addClass("form-control");
+        $("#app > div:nth-child(2) > div > div:nth-child(1) > input").addClass("form-control");
+    });
 </script> 
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,10 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     
-    <!-- Jquery -->
-    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
 </head>
 <body>
     <div id="app">
@@ -78,8 +80,9 @@
         <main class="py-4">
             @yield('content')
 
-            @yield('script')
+            
         </main>
     </div>
+    @yield('script')
 </body>
 </html>
